41c0ef8bebb58e0ac0ecdae695b1c45ef9311d59
ModestArtitic <modestartistic100@gmail.com>
2023-09-14 20:59:33 +0700

    commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function tenHam(){
+    const font2 = document.querySelector(".font2")
+    console.log(font2)
+    font2.classList.toggle("hide")
+
+    
+}
+const bagach = document.querySelector(".back9")
+console.log(bagach)
+bagach.onclick = tenHam
